fb751e096bbd35703155384cbfb63b5a6b1451e2
jdab2020 <jdabalos90@gmail.com>
2020-03-01 10:45:14 -0800

    populated schema and seed

---
+++ seeds.sql
@@ -0,0 +1,10 @@
+USE burger_cravings;
+
+INSERT INTO burgers 
+    (burger_name)
+VALUES
+    ("The Fast Burger"),
+    ("Vegetarian HIHO"),
+    ("Double Cheeseburger"),
+    ("Steak Burger"),
+    ("Office Burger");
