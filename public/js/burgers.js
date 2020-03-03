b755ba10d917575ddd68e38d5a149b4da5d9554b
jdab2020 <jdabalos90@gmail.com>
2020-03-03 01:04:13 -0800

    updated handlebars | updated click handlers

--- public/js/burgers.js
+++ public/js/burgers.js
@@ -1,6 +1,25 @@
 // click handler
 $(document).ready(function () {
-    $(".devour").on("click", function (event){
-        event.preventDefault();
-    })
+    $(document).on("click","button.add",addBurger); // add button handler
+    $(document).on("click","button.devour",devourBurger); // devour button handler
+    
+    devourBurger = () => {
+        let id = $(this).data("id");
+        let newState = $(this).data("devour")
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newState
+        }).then(() => {location.reload();})
+    }
+
+    addBurger = () => {
+        let id = $(this).data("id");
+        
+        $.ajax({
+            type: "POST",
+            data: 
+        }).then(()=>{location.reload();})
+    }
+
 })
