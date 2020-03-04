95b5076f742636098a050990ccc63b9d124b2a2b
jdab2020 <jdabalos90@gmail.com>
2020-03-04 12:38:18 -0800

    final deploying

--- config/config.js
+++ config/config.js
@@ -22,7 +22,8 @@ module.exports =
       "database": "database_production",
       "host": "127.0.0.1",
       "dialect": "mysql",
-      "operatorsAliases": false
+      "operatorsAliases": false,
+      "use_env_variable": "JAWSDB_URL"
     }
   }
   
